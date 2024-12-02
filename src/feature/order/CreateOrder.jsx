bce4831bcbafe980ca8945c78a75065fc740d7e7
naveenkumar13-gif <naveenkumarofficial1316@gmail.com>
2024-12-02 21:20:16 +0530

    commit

--- src/feature/order/CreateOrder.jsx
+++ src/feature/order/CreateOrder.jsx
@@ -32,7 +32,6 @@ function CreateOrder() {
   return (
     <div className="px-4 py-6">
       <h2 className="mb-8 text-xl font-semibold">Ready to order? Let's go!</h2>
-      <button onClick={() => dispatch(fetchAddress())}>get Postion</button>
       <Form method="POST">
         <div className="box">
           <label className="sm:basis-40">First Name</label>
